817845679d1f3a453b7878634536d47a3c662191
Sebastián <sebastiaan.peerez@gmail.com>
2026-02-15 20:15:06 -0300

    Change and Order the lines

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 import express from 'express';
 const app = express();
 
-app.use((req, res, next) => {
-  // res.json({ 'message': 'Hola Middleware' });
-  console.log(req.method);
-  next();
-});
+// app.use((req, res, next) => {});
 
 app.get('/', (req, res) => {
   res.json({ "message": "¡Bienvenidos a nuestra API REST!" });
 });
 
-import notFoundMiddleware from './src/middlewares/not-found.js';
-app.use(notFoundMiddleware);
+import notFound from './src/middlewares/not-found.js';
+app.use(notFound);
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
 
