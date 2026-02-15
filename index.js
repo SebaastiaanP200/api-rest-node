30933b90cc5d2de4cf03a7f579da4648c0f8a886
Sebastián <sebastiaan.peerez@gmail.com>
2026-02-15 20:21:17 -0300

    Change Not Found

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 const app = express();
+const PORT = 3000;
 
-// app.use((req, res, next) => {});
+app.use((req, res, next) => {
+  // console.log(req.method);
+  // next();
+});
 
 app.get('/', (req, res) => {
   res.json({ "message": "¡Bienvenidos a nuestra API REST!" });
 });
 
-import notFound from './src/middlewares/not-found.js';
-app.use(notFound);
+import notFoundMiddleware from './src/middlewares/not-found.js';
+app.use(notFoundMiddleware);
 
-const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
 
